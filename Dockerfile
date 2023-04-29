449635b229de6d63392a4ca66f122ab220455f17
Ed Asriyan <github@asriyan.me>
2023-04-29 13:39:31 +0400

    Simplify the tool

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM python:3.8
+FROM python:3.8-buster
 RUN apt update && apt install -y portaudio19-dev python3-pyaudio libsndfile1-dev
 
 WORKDIR /app
 ADD requirements.txt .
 RUN pip install -r requirements.txt
 
-ADD results results
-ADD utils.py .
+ADD model.h5 .
 ADD recognize.py .
 
 ENTRYPOINT ["python", "recognize.py", "--file"]
--- recognize.py
+++ recognize.py
@@ -1,121 +1,31 @@
-import pyaudio
-import wave
-import librosa
-import numpy as np
-from sys import byteorder
-from array import array
-from struct import pack
+from enum import Enum
 
-THRESHOLD = 500
-CHUNK_SIZE = 1024
-FORMAT = pyaudio.paInt16
-RATE = 16000
+from librosa import core, stft, effects, feature
+from numpy import abs, mean, hstack, array
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LSTM, Dropout
 
-SILENCE = 30
 
-def is_silent(snd_data):
-    "Returns 'True' if below the 'silent' threshold"
-    return max(snd_data) < THRESHOLD
-
-def normalize(snd_data):
-    "Average the volume out"
-    MAXIMUM = 16384
-    times = float(MAXIMUM)/max(abs(i) for i in snd_data)
-
-    r = array('h')
-    for i in snd_data:
-        r.append(int(i*times))
-    return r
-
-def trim(snd_data):
-    "Trim the blank spots at the start and end"
-    def _trim(snd_data):
-        snd_started = False
-        r = array('h')
-
-        for i in snd_data:
-            if not snd_started and abs(i)>THRESHOLD:
-                snd_started = True
-                r.append(i)
-
-            elif snd_started:
-                r.append(i)
-        return r
-
-    # Trim to the left
-    snd_data = _trim(snd_data)
-
-    # Trim to the right
-    snd_data.reverse()
-    snd_data = _trim(snd_data)
-    snd_data.reverse()
-    return snd_data
-
-def add_silence(snd_data, seconds):
-    "Add silence to the start and end of 'snd_data' of length 'seconds' (float)"
-    r = array('h', [0 for i in range(int(seconds*RATE))])
-    r.extend(snd_data)
-    r.extend([0 for i in range(int(seconds*RATE))])
-    return r
-
-def record():
-    """
-    Record a word or words from the microphone and 
-    return the data as an array of signed shorts.
-    Normalizes the audio, trims silence from the 
-    start and end, and pads with 0.5 seconds of 
-    blank sound to make sure VLC et al can play 
-    it without getting chopped off.
-    """
-    p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=1, rate=RATE,
-        input=True, output=True,
-        frames_per_buffer=CHUNK_SIZE)
-
-    num_silent = 0
-    snd_started = False
-
-    r = array('h')
-
-    while 1:
-        # little endian, signed short
-        snd_data = array('h', stream.read(CHUNK_SIZE))
-        if byteorder == 'big':
-            snd_data.byteswap()
-        r.extend(snd_data)
-
-        silent = is_silent(snd_data)
-
-        if silent and snd_started:
-            num_silent += 1
-        elif not silent and not snd_started:
-            snd_started = True
-
-        if snd_started and num_silent > SILENCE:
-            break
-
-    sample_width = p.get_sample_size(FORMAT)
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    r = normalize(r)
-    r = trim(r)
-    r = add_silence(r, 0.5)
-    return sample_width, r
-
-def record_to_file(path):
-    "Records from the microphone and outputs the resulting data to 'path'"
-    sample_width, data = record()
-    data = pack('<' + ('h'*len(data)), *data)
-
-    wf = wave.open(path, 'wb')
-    wf.setnchannels(1)
-    wf.setsampwidth(sample_width)
-    wf.setframerate(RATE)
-    wf.writeframes(data)
-    wf.close()
-
+def create_model(vector_length=128):
+    """5 hidden dense layers from 256 units to 64, not the best model, but not bad."""
+    model = Sequential()
+    model.add(Dense(256, input_shape=(vector_length,)))
+    model.add(Dropout(0.3))
+    model.add(Dense(256, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation="relu"))
+    model.add(Dropout(0.3))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.3))
+    # one output neuron with sigmoid activation function, 0 means female, 1 means male
+    model.add(Dense(1, activation="sigmoid"))
+    # using binary crossentropy as it's male/female classification (binary)
+    model.compile(loss="binary_crossentropy", metrics=["accuracy"], optimizer="adam")
+    # load the saved/trained weights
+    model.load_weights("model.h5")
+    return model
 
 
 def extract_feature(file_name, **kwargs):
@@ -135,46 +45,48 @@ def extract_feature(file_name, **kwargs):
     mel = kwargs.get("mel")
     contrast = kwargs.get("contrast")
     tonnetz = kwargs.get("tonnetz")
-    X, sample_rate = librosa.core.load(file_name)
+    X, sample_rate = core.load(file_name)
     if chroma or contrast:
-        stft = np.abs(librosa.stft(X))
-    result = np.array([])
+        stft = abs(stft(X))
+    result = array([])
     if mfcc:
-        mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
-        result = np.hstack((result, mfccs))
+        mfccs = mean(feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
+        result = hstack((result, mfccs))
     if chroma:
-        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-        result = np.hstack((result, chroma))
+        chroma = mean(feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+        result = hstack((result, chroma))
     if mel:
-        mel = np.mean(librosa.feature.melspectrogram(y=X, sr=sample_rate).T,axis=0)
-        result = np.hstack((result, mel))
+        mel = mean(feature.melspectrogram(y=X, sr=sample_rate).T, axis=0)
+        result = hstack((result, mel))
     if contrast:
-        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-        result = np.hstack((result, contrast))
+        contrast = mean(feature.spectral_contrast(S=stft, sr=sample_rate).T, axis=0)
+        result = hstack((result, contrast))
     if tonnetz:
-        tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
-        result = np.hstack((result, tonnetz))
+        tonnetz = mean(feature.tonnetz(y=effects.harmonic(X), sr=sample_rate).T, axis=0)
+        result = hstack((result, tonnetz))
     return result
+
+
+class Gender(Enum):
+    Male = 0
+    Female = 1
+
+class Recognizer:
+    def __init__(self):
+        self._model = create_model()
+
+    def recognize(self, path: str) -> Gender:
+        features = extract_feature(path, mel=True).reshape(1, -1)
+        return Gender.Male if self._model.predict(features, verbose=0)[0][0] >= 0.5 else Gender.Female
 
 
 if __name__ == "__main__":
     # load the saved model (after training)
     # model = pickle.load(open("result/mlp_classifier.model", "rb"))
-    from utils import create_model
     import argparse
+
     parser = argparse.ArgumentParser(description="Gender recognition script that returns MALE or FEMALE")
     parser.add_argument("-f", "--file", help="The path to the file, preferred to be in WAV format")
     args = parser.parse_args()
-    file = args.file
-    # construct the model
-    model = create_model()
-    # load the saved/trained weights
-    model.load_weights("results/model.h5")
-    # extract features and reshape it
-    features = extract_feature(file, mel=True).reshape(1, -1)
-    # predict the gender!
-    male_prob = model.predict(features, verbose=0)[0][0]
-    female_prob = 1 - male_prob
-    # show the result!
-    result = "male" if male_prob > female_prob else "female"
+    result = "male" if Recognizer().recognize(args.file) == Gender.Male else "female"
     print(result)
